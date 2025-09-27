e5d036db0ac49cb4a87f756e43dc5d9365b4ebdd
faly9 <randriantsoafaly8@gmail.com>
2025-09-27 06:52:01 +0000

    routing for mission

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import backend.routing  # importe ton fichier routing
+import mission.routing   # importe ton fichier routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
@@ -10,7 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            backend.routing.websocket_urlpatterns
+            mission.routing.websocket_urlpatterns
         )
     ),
 })
--- mission/routing.py
+++ mission/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from mission import consumers
+from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/missions/$", consumers.MissionConsumer.as_asgi()),
