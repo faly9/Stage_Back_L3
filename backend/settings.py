8fc6496fbab3727d9b041abefb55146bb8ddbd75
faly9 <randriantsoafaly8@gmail.com>
2025-10-30 07:51:10 +0000

    l

--- backend/settings.py
+++ backend/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 # Fichiers statiques et médias
 # -------------------------------
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # -------------------------------
