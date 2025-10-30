7d3d1e451363a71f85a72c05158726080d3e5252
faly9 <randriantsoafaly8@gmail.com>
2025-10-30 13:35:10 +0000

    f

--- freelance/views.py
+++ freelance/views.py
@@ -3,13 +3,10 @@ from rest_framework import viewsets, permissions
 from rest_framework.exceptions import PermissionDenied
 from .models import Freelance
 from .serializers import FreelanceSerializer
-from rest_framework.parsers import MultiPartParser, FormParser
-
 
 class FreelanceViewSet(viewsets.ModelViewSet):
     serializer_class = FreelanceSerializer
     permission_classes = [permissions.IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]
 
     def get_queryset(self):
         # Chaque utilisateur ne peut voir que SON freelance
@@ -27,13 +24,6 @@ class FreelanceViewSet(viewsets.ModelViewSet):
         if freelance.user != self.request.user:
             raise PermissionDenied("Vous n’êtes pas autorisé à modifier ce profil.")
         serializer.save()
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)  # 🔹 partial=True
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
 
     def perform_destroy(self, instance):
         # Vérification que l'utilisateur supprime son propre profil
