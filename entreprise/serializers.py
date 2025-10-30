459200cac016983f4399a5fc58847e5afba8da07
faly9 <randriantsoafaly8@gmail.com>
2025-10-30 10:08:31 +0000

    l

--- entreprise/serializers.py
+++ entreprise/serializers.py
@@ -4,8 +4,14 @@ from .models import Entreprise
 
 class EntrepriseSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source="user.email", read_only=True)  # <-- ajoute l'email ici
-    profile_image = serializers.ImageField(use_url=True, required=False, allow_null=True)
+    profile_image = serializers.SerializerMethodField()
     class Meta:
         model = Entreprise
         fields = ['id_entreprise', 'nom', 'secteur', 'user', 'profile_image', 'email']
         read_only_fields = ['id_entreprise', 'user', 'email']
+        
+    def get_profile_image(self, obj):
+        if obj.profile_image:
+            # renvoie uniquement le chemin relatif
+            return obj.profile_image.url.replace(obj.profile_image.storage.base_url, '')  
+        return None
