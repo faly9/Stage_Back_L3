1d20ebe29d58b578144c54c1c8a776fd499b5b5e
faly9 <randriantsoafaly8@gmail.com>
2025-10-30 09:00:18 +0000

    k

--- entreprise/serializers.py
+++ entreprise/serializers.py
@@ -4,7 +4,7 @@ from .models import Entreprise
 
 class EntrepriseSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source="user.email", read_only=True)  # <-- ajoute l'email ici
-
+    profile_image = serializers.ImageField(use_url=True, required=False, allow_null=True)
     class Meta:
         model = Entreprise
         fields = ['id_entreprise', 'nom', 'secteur', 'user', 'profile_image', 'email']
--- freelance/serializers.py
+++ freelance/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from .models import Freelance
 
 class FreelanceSerializer(serializers.ModelSerializer):
-    freelance_email = serializers.EmailField(source="freelance.user.email", read_only=True)
+    freelance_email = serializers.EmailField(source="user.email", read_only=True)
+    photo = serializers.SerializerMethodField()  # champ personnalisé pour photo
+
     class Meta:
         model = Freelance
         fields = "__all__"
-        read_only_fields = ["id_freelance", "user", "date_creation","freelance_email"]
+        read_only_fields = ["id_freelance", "user", "date_creation", "freelance_email"]
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return obj.photo.name  # renvoie seulement le chemin relatif /media/...
+        return None
