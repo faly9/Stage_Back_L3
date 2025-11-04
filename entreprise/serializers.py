7ca43cfcd5745ac89d0de77707dbf0ad4e136ff2
faly9 <randriantsoafaly8@gmail.com>
2025-11-04 15:03:59 +0000

    sary

--- entreprise/serializers.py
+++ entreprise/serializers.py
@@ -3,9 +3,16 @@ from rest_framework import serializers
 from .models import Entreprise
 
 class EntrepriseSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(source="user.email", read_only=True)  # <-- ajoute l'email ici
+    email = serializers.CharField(source="user.email", read_only=True)
 
     class Meta:
         model = Entreprise
         fields = ['id_entreprise', 'nom', 'secteur', 'user', 'profile_image', 'email']
         read_only_fields = ['id_entreprise', 'user', 'email']
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        # 🔹 garder seulement le chemin relatif pour profile_image
+        if rep.get("profile_image"):
+            rep["profile_image"] = instance.profile_image.name  # chemin relatif
+        return rep
--- freelance/serializers.py
+++ freelance/serializers.py
@@ -3,7 +3,34 @@ from .models import Freelance
 
 class FreelanceSerializer(serializers.ModelSerializer):
     freelance_email = serializers.EmailField(source="freelance.user.email", read_only=True)
+    photo = serializers.ImageField(required=False, allow_null=True)
     class Meta:
         model = Freelance
-        fields = "__all__"
+        fields = [
+            "id_freelance",
+            "user",
+            "nom",
+            "description",
+            "competence",
+            "experience",
+            "formation",
+            "certificat",
+            "tarif",
+            "photo",
+            "date_creation",
+            "freelance_email",
+        ]
         read_only_fields = ["id_freelance", "user", "date_creation","freelance_email"]
+
+    def to_representation(self, instance):
+        """Retourner le chemin relatif pour la photo"""
+        rep = super().to_representation(instance)
+        if rep.get("photo"):
+            rep["photo"] = instance.photo.name  # <-- chemin relatif
+        return rep
+
+    # def get_photo(self, obj):
+    #     if obj.photo:
+    #         photo_url = obj.photo.url
+    #         return photo_url
+    #     return None 
